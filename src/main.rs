5f33ce444c8b0e01289e069f5656f4cc3ca68dc7
MichaelWang <michael_wangguanyi@163.com>
2024-10-28 12:36:07 +0800

    add scope for all routes

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,29 @@ async fn echo(req_body: String) -> impl Responder {
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there! This is manual hello")
 }
+
+async fn index() -> impl Responder {
+    "michael index"
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // HttpServer::new(|| {
+    //     App::new()
+    //         .service(say_hello)
+    //         .service(echo)
+    //         .route("/manual_route", web::get().to(manual_hello))
+    // })
+    // .bind(("127.0.0.1", 8080))?
+    // .run()
+    // .await
     HttpServer::new(|| {
-        App::new()
-            .service(say_hello)
-            .service(echo)
-            .route("/manual_route", web::get().to(manual_hello))
+        App::new().service(
+            web::scope("michael")
+                .service(say_hello)
+                .service(echo)
+                .route("/index.html", web::get().to(index)),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
